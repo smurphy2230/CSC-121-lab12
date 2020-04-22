caaf4bfd1c11e54a39130c5c22ba15f291cc9ca8
smurphy2230 <smurphy2230@gmail.com>
2020-04-22 12:35:16 -0400

    starting on p2

---
+++ manage_course/course.py
@@ -0,0 +1,7 @@
+class Course:
+
+    def __init__(self, course_code, max_class_size):
+
+        self.course_code = course_code
+        self.max_class_size = max_class_size
+        self.enrollment = 0.0
